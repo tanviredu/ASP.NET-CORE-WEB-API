2ecec588e6567f259aabfdc6df6854c494a4018b
tanviredu <ornobtanvir.git@gmail.com>
2020-11-09 23:24:37 +0600

    ADDED

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "mongourl" : "mongodb+srv://trahman123456:trahman123456@cluster0.v1x5w.mongodb.net/todo?retryWrites=true&w=majority",
+    "port": process.env.PORT || 3000,
+    "secrets": process.env.JWT || "secret",
+    "expiretime":24*60*10
+}
